9f3a2b9b11c6a49f55f989548db30a610208c6cd
Jatery <cyz3827@gmail.com>
2023-09-26 22:32:52 +0800

    jg50231 initialize

---
+++ judgegirl/jg50231/jg50231.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define DEBUG
+
+#define f(k) (77 * k + 2222) % s
+#define maxn 105
+#define maxs maxn * maxn
+#define maxc 10
+#define maxk 2e7
+
+int main() {
+	int n, s, c;
+	scanf("%d%d%d", &n, &s, &c);
+	
+	assert(n <= maxn);
+
+	int tower[n][n][n];
+
+	
+}
